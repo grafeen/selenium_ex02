5defd69772411a7052c73aaf8dc284fbe8ee68f9
goodybox <grafeen37@gmail.com>
2021-12-13 22:09:06 +0900

    Selenium webdriver 로 웹페이지 띄우기 (2) https://easytesting.tistory.com/5

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url='https://www.google.com/')
+
+driver.quit()
